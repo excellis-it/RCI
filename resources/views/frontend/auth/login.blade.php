5922e986a6b671c4927faa95678cd57271ee80d4
unknown <shreeja@excellisit.net>
2024-05-03 12:31:30 +0530

    many chnages

--- resources/views/frontend/auth/login.blade.php
+++ resources/views/frontend/auth/login.blade.php
@@ -26,6 +26,10 @@
 
 <body>
 
+    @php
+        use App\Helpers\Helper;
+    @endphp
+
     <!--  Body Wrapper -->
     <div class="login_bg">
 
@@ -37,8 +41,13 @@
                     <div class="card w-100">
                         <div class="card-body">
                             <a href="javascript:void(0);" class="text-nowrap d-block text-center mx-auto logo-img mb-4">
-                                <img src="{{ asset('frontend_assets/images/logo.png') }}" class="dark-logo"
+                                @if (!isset(Helper::logo()->logo))                              
+                                    <img src="{{ asset('frontend_assets/images/logo.png') }}" class="dark-logo"
                                     alt="">
+                                @else
+                                    <img src="{{ Storage::url(Helper::logo()->logo) }}" class="dark-logo" alt="">
+    
+                                @endif
                             </a>
                             <form action="{{ route('login-check') }}" method="POST">
                                 @csrf
