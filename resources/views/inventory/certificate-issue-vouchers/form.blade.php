c59c20eb7d318f85b5938308b96406b3343b6985
excellis-it <web.excellisit@gmail.com>
2024-05-10 18:07:02 +0530

    changes

--- resources/views/inventory/certificate-issue-vouchers/form.blade.php
+++ resources/views/inventory/certificate-issue-vouchers/form.blade.php
@@ -8,13 +8,30 @@
                     <div class="form-group col-md-4 mb-2">
                         <div class="row align-items-center">
                             <div class="col-md-12">
+                                <label>Person Name </label>
+                            </div>
+                            <div class="col-md-12">
+                                <select class="form-select" name="member_id" id="member_id">
+                                    <option value="">Select Name </option>
+                                    @foreach ($members as $member)
+                                        <option value="{{ $member->id }}" {{ $member->id == $certificateIssueVoucher->member_id ? 'selected' : '' }}>{{ $member->code }}</option>
+                                        
+                                    @endforeach
+                                </select>
+                                <span class="text-danger"></span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="form-group col-md-4 mb-2">
+                        <div class="row align-items-center">
+                            <div class="col-md-12">
                                 <label>Item Code  </label>
                             </div>
                             <div class="col-md-12">
                                 <select class="form-select" name="item_id" id="item_id">
                                     <option value="">Select Item Code </option>
                                     @foreach ($items as $item)
-                                        <option value="{{ $item->id }}" {{ ($rin->item_id == $item->id) ? 'selected' : '' }}>{{ $item->code }}</option>
+                                        <option value="{{ $item->id }}" {{ $item->id == $certificateIssueVoucher->item_id ? 'selected' : '' }}>{{ $item->code }}</option>
                                         
                                     @endforeach
                                 </select>
@@ -26,99 +43,50 @@
                     <div class="form-group col-md-4 mb-2">
                         <div class="row align-items-center">
                             <div class="col-md-12">
+                                <label>Price</label>
+                            </div>
+                            <div class="col-md-12">
+                                <input type="text" class="form-control" name="price" id="price" value="{{ $certificateIssueVoucher->price }}"
+                                    placeholder="">
+                                <span class="text-danger"></span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="form-group col-md-4 mb-2">
+                        <div class="row align-items-center">
+                            <div class="col-md-12">
+                                <label>Item Type</label>
+                            </div>
+                            <div class="col-md-12">
+                                <select class="form-select" name="item_type" id="item_type">
+                                    <option value="">Select Item Type</option>
+                                    @foreach ($items as $itemType)
+                                        <option value="{{ $itemType->item_type }}" {{ $itemType->itemType == $certificateIssueVoucher->item_type ? 'selected' : '' }}>{{ $itemType->item_type }}</option>
+                                        
+                                    @endforeach
+                                </select>
+                                <span class="text-danger"></span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="form-group col-md-4 mb-2">
+                        <div class="row align-items-center">
+                            <div class="col-md-12">
                                 <label>Description</label>
                             </div>
                             <div class="col-md-12">
-                                <input type="text" class="form-control" name="description" id="description" value="{{ $rin->description }}"
+                                <input type="text" class="form-control" name="description" id="description" value="{{ $certificateIssueVoucher->description }}"
                                     placeholder="">
                                 <span class="text-danger"></span>
                             </div>
                         </div>
+                        
                     </div>
-                    <div class="form-group col-md-4 mb-2">
-                        <div class="row align-items-center">
-                            <div class="col-md-12">
-                                <label>Received Quantity</label>
-                            </div>
-                            <div class="col-md-12">
-                                <input type="text" class="form-control" name="received_quantity" id="edit_received_quantity" value="{{ $rin->received_quantity }}"
-                                    placeholder="">
-                                <span class="text-danger"></span>
-                            </div>
-                        </div>
-                    </div>
+                    
                 </div>
-                <div class="row">
-                    <div class="form-group col-md-4 mb-2">
-                        <div class="row align-items-center">
-                            <div class="col-md-12">
-                                <label>Accepted Quantity</label>
-                            </div>
-                            <div class="col-md-12">
-                                <input type="text" class="form-control" name="accepted_quantity" id="edit_accepted_quantity"  value="{{ $rin->accepted_quantity }}"
-                                    placeholder="">
-                                <span class="text-danger"></span>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="form-group col-md-4 mb-2">
-                        <div class="row align-items-center">
-                            <div class="col-md-12">
-                                <label>Rejected Quantity</label>
-                            </div>
-                            <div class="col-md-12">
-                                <input type="text" class="form-control" name="rejected_quantity" id="edit_rejected_quantity"  value="{{ $rin->rejected_quantity }}"
-                                    placeholder="" readonly>
-                                <span class="text-danger"></span>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="form-group col-md-4 mb-2">
-                        <div class="row align-items-center">
-                            <div class="col-md-12">
-                                <label>Remarks</label>
-                            </div>
-                            <div class="col-md-12">
-                                <input type="text" class="form-control" name="remarks" id="remarks" value="{{ $rin->remarks }}"
-                                    placeholder="">
-                                <span class="text-danger"></span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="form-group col-md-4 mb-2">
-                        <div class="row align-items-center">
-                            <div class="col-md-12">
-                                <label>NC Status</label>
-                            </div>
-                            <div class="col-md-12">
-                                <select class="form-select" name="nc_status" id="nc_status">
-                                    <option value="">Select NC Status</option>
-                                    <option value="C" {{ ($rin->nc_status == 'C') ? 'selected' : '' }}>C</option>
-                                    <option value="NC" {{ ($rin->nc_status == 'NC') ? 'selected' : '' }}>NC</option>
-                                    <option value="NCF" {{ ($rin->nc_status == 'NCF') ? 'selected' : '' }}>NCF</option>
-                                </select>
-                                <span class="text-danger"></span>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="form-group col-md-4 mb-2">
-                        <div class="row align-items-center">
-                            <div class="col-md-12">
-                                <label>A/U Status</label>
-                            </div>
-                            <div class="col-md-12">
-                                <select class="form-select" name="au_status" id="au_status">
-                                    <option value="">Select A/U Status</option>
-                                    <option value="yes" {{ ($rin->au_status == 'yes') ? 'selected' : '' }}>Yes</option>
-                                    <option value="no" {{ ($rin->au_status == 'no') ? 'selected' : '' }}>No</option>
-                                </select>
-                                <span class="text-danger"></span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                
             </div>
             <div class="col-md-2">
                 <div class="mb-1">
@@ -202,79 +170,22 @@
                             </div>
                         </div>
                     </div>
-                </div>
-                <div class="row">
                     <div class="form-group col-md-4 mb-2">
                         <div class="row align-items-center">
                             <div class="col-md-12">
-                                <label>Accepted Quantity</label>
+                                <label>Description</label>
                             </div>
                             <div class="col-md-12">
-                                <input type="text" class="form-control" name="accepted_quantity" id="accepted_quantity" value=""
+                                <input type="text" class="form-control" name="description" id="description" value=""
                                     placeholder="">
                                 <span class="text-danger"></span>
                             </div>
                         </div>
                         
                     </div>
-                    <div class="form-group col-md-4 mb-2">
-                        <div class="row align-items-center">
-                            <div class="col-md-12">
-                                <label>Rejected Quantity</label>
-                            </div>
-                            <div class="col-md-12">
-                                <input type="text" class="form-control" name="rejected_quantity" id="rejected_quantity" value=""
-                                    placeholder="" readonly>
-                                <span class="text-danger"></span>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="form-group col-md-4 mb-2">
-                        <div class="row align-items-center">
-                            <div class="col-md-12">
-                                <label>Remarks</label>
-                            </div>
-                            <div class="col-md-12">
-                                <input type="text" class="form-control" name="remarks" id="remarks" value=""
-                                    placeholder="">
-                                <span class="text-danger"></span>
-                            </div>
-                        </div>
-                    </div>
+                    
                 </div>
-                <div class="row">
-                    <div class="form-group col-md-4 mb-2">
-                        <div class="row align-items-center">
-                            <div class="col-md-12">
-                                <label>NC Status</label>
-                            </div>
-                            <div class="col-md-12">
-                                <select class="form-select" name="nc_status" id="nc_status">
-                                    <option value="">Select NC Status</option>
-                                    <option value="C">C</option>
-                                    <option value="NC">NC</option>
-                                    <option value="NCF">NCF</option>
-                                </select>
-                                <span class="text-danger"></span>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="form-group col-md-4 mb-2">
-                        <div class="row align-items-center">
-                            <div class="col-md-12">
-                                <label>A/U Status</label>
-                            </div>
-                            <div class="col-md-12">
-                                <select class="form-select" name="au_status" id="au_status">
-                                    <option value="">Select A/U Status</option>
-                                    <option value="yes">Yes</option>
-                                    <option value="no">No</option>
-                                </select>
-                                <span class="text-danger"></span>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                
             </div>
             <div class="col-md-2">
                 <div class="mb-1">
--- resources/views/inventory/certificate-issue-vouchers/list.blade.php
+++ resources/views/inventory/certificate-issue-vouchers/list.blade.php
@@ -1,6 +1,6 @@
 @extends('inventory.layouts.master')
 @section('title')
-    RIN List
+    Certificate Issue Voucher List
 @endsection
 
 @push('styles')
@@ -15,10 +15,10 @@
             <div class="d-flex">
                 <div class="arrow_left"><a href="" class="text-white"><i class="ti ti-arrow-left"></i></a></div>
                 <div class="">
-                    <h3>RIN Listing</h3>
+                    <h3>Certificate Issue Voucher Listing</h3>
                     <ul class="breadcome-menu mb-0">
                         <li><a href="#">Home</a> <span class="bread-slash">/</span></li>
-                        <li><span class="bread-blod">RIN Listing</span></li>
+                        <li><span class="bread-blod">Certificate Issue Voucher Listing</span></li>
                     </ul>
                 </div>
             </div>
@@ -30,7 +30,7 @@
                 <div class="card w-100">
                     <div class="card-body">
                         <div id="form">
-                            @include('inventory.rins.form')
+                            @include('inventory.certificate-issue-vouchers.form')
                         </div>
 
                         <div class="row">
@@ -49,10 +49,10 @@
                                         <thead class="text-white fs-4 bg_blue">
                                             <tr>
                                                 <th>SL No.</th>
-                                                <th class="sorting" data-sorting_type="desc" data-column_name="item_id"
-                                                    style="cursor: pointer"> Item Code <span id="item_id_icon"></span> </th>
+                                                <th class="sorting" data-sorting_type="desc" data-column_name="name"
+                                                    style="cursor: pointer"> Name <span id="name_icon"></span> </th>
                                                 {{-- <th>Code</th> --}}
-                                                <th>Received Quantity</th>
+                                                <th class="sorting" data-sorting_type="desc">Received Quantity</th>
                                                 <th>Accepted Quantity</th>
                                                 <th>Rejected Quantity</th>
                                                 <th class="sorting" data-sorting_type="desc" data-column_name="nc_status"
@@ -63,7 +63,7 @@
                                             </tr>
                                         </thead>
                                         <tbody class="tbody_height_scroll">
-                                            @include('inventory.rins.table')
+                                            @include('inventory.certificate-issue-vouchers.table')
                                         </tbody>
                                     </table>
                                     <input type="hidden" name="hidden_page" id="hidden_page" value="1" />
