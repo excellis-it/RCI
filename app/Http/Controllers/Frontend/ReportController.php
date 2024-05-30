055e9214429182664126453e9f0eb6fa3e7d6064
shreeja <shreeja@excellisit.net>
2024-05-30 12:59:56 -0700

    pdf

--- app/Http/Controllers/Frontend/ReportController.php
+++ app/Http/Controllers/Frontend/ReportController.php
@@ -8,6 +8,8 @@ use App\Models\Member;
 use App\Models\MemberCoreInfo;
 use App\Models\MemberCredit;
 use App\Models\MemberDebit;
+use App\Models\MemberRecovery;
+use PDF;
 
 class ReportController extends Controller
 {
@@ -21,14 +23,25 @@ class ReportController extends Controller
 
     public function payslipGenerate(Request $request)
     {
+        $request->validate([
+            'member_id' => 'required',
+            'month' => 'required',
+            'year' => 'required',
+        ]);
+
         $member_data = Member::where('id', $request->member_id)->first();
         $member_credit_data = MemberCredit::where('member_id', $request->member_id)->first();
         $member_debit_data = MemberDebit::where('member_id', $request->member_id)->first();
         $member_core_info = MemberCoreInfo::where('member_id', $request->member_id)->first();
+        $member_recoveries_data = MemberRecovery::where('member_id', $request->member_id)->first();
+        $month = $request->month;
+        $dateObj   = \DateTime::createFromFormat('!m', $month);
+        $monthName = $dateObj->format('F');
+        $year = $request->year;
         
-        // dd($member_data, $member_credit_data, $member_debit_data);
-
-        return view('frontend.reports.payslip-generate', compact('member_data', 'member_credit_data', 'member_debit_data', 'member_core_info'));
+        $pdf = PDF::loadView('frontend.reports.payslip-generate', compact('member_data', 'member_credit_data', 'member_debit_data', 'member_core_info', 'monthName', 'year'));
+        return $pdf->download('document.pdf');
+    
     }
 
     public function crv()
