ba998dcfad1b63d9447e565cbfa5778769f3a475
shreeja <shreeja@excellisit.net>
2024-08-02 18:39:21 -0700

    many chnages

---
+++ database/migrations/2024_08_02_203928_add_pc_project_to_cda_bill_audit_teams_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('cda_bill_audit_teams', function (Blueprint $table) {
+            $table->integer('pc_no')->nullable()->after('adv_amount');
+            $table->integer('firm_name')->nullable()->after('crv_no');
+            $table->integer('cda_bill_no')->nullable()->after('firm_name');
+            $table->integer('cda_bill_date')->nullable()->after('cda_bill_no');
+            $table->integer('cda_bill_amount')->nullable()->after('cda_bill_no');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('cda_bill_audit_teams', function (Blueprint $table) {
+            //
+            $table->dropColumn('pc_no');
+            $table->dropColumn('firm_name');
+            $table->dropColumn('cda_bill_no');
+            $table->dropColumn('cda_bill_date');
+            $table->dropColumn('cda_bill_amount');
+        });
+    }
+};
---
+++ resources/views/imprest/reports/out-standing-report-generate.blade.php
@@ -0,0 +1,368 @@
+<!DOCTYPE html>
+<html lang="en">
+<title>RCI</title>
+<meta charset="utf-8" />
+<style>
+    @page {
+        size: 29.7cm 42cm
+    }
+</style>
+
+<body style="background: #fff">
+  <table width="100%" border="0" cellpadding="0" cellspacing="0" bgcolor="#ffffff"
+    style="border-radius: 0px; margin: 0 auto">
+    <tbody>
+      <tr>
+        <td style="padding: 0 0px">
+          <table width="100%" border="0" cellpadding="0" cellspacing="0" align="center">
+            <tbody>
+              <tr>
+                <td style="
+                      font-size: 10px;
+                      line-height: 14px;
+                      font-weight: 600;
+                      color: #000;
+                      text-align: center;
+                      padding: 0px 5px !important;
+                      margin: 0px 0px !important;
+                      border: 1px solid #000;
+                    ">
+                Outstanding Bills	as on 17/01/2024
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </td>
+      </tr> 
+      <tr>
+        <td style="padding: 0 0px">
+          <table width="100%" border="0" cellpadding="0" cellspacing="0" align="center">
+            <thead>
+              <tr>
+                <td style="
+                      font-size: 10px;
+                      line-height: 14px;
+                      font-weight: 400;
+                      color: #000;
+                      text-align: center;
+                      padding: 0px 5px !important;
+                      margin: 0px 0px !important;
+                      border: 1px solid #000;
+                      height: 5px;
+                    ">
+                    Sr No
+                </td>
+                <td style="
+                      font-size: 10px;
+                      line-height: 14px;
+                      font-weight: 400;
+                      color: #000;
+                      text-align: center;
+                      padding: 0px 5px !important;
+                      margin: 0px 0px !important;
+                      border: 1px solid #000;
+                      height: 5px;
+                    ">
+                    PC No
+                </td>
+                <td style="
+                      font-size: 10px;
+                      line-height: 14px;
+                      font-weight: 400;
+                      color: #000;
+                      text-align: center;
+                      padding: 0px 5px !important;
+                      margin: 0px 0px !important;
+                      border: 1px solid #000;
+                      height: 5px;
+                    ">
+                   Project
+                </td>
+                <td style="
+                      font-size: 10px;
+                      line-height: 14px;
+                      font-weight: 400;
+                      color: #000;
+                      text-align: center;
+                      padding: 0px 5px !important;
+                      margin: 0px 0px !important;
+                      border: 1px solid #000;
+                      height: 5px;
+                    ">
+                    ADV No
+                </td>
+                <td style="
+                      font-size: 10px;
+                      line-height: 14px;
+                      font-weight: 400;
+                      color: #000;
+                      text-align: center;
+                      padding: 0px 5px !important;
+                      margin: 0px 0px !important;
+                      border: 1px solid #000;
+                      height: 5px;
+                    ">
+                   ADV Date
+                </td>
+                <td style="
+                      font-size: 10px;
+                      line-height: 14px;
+                      font-weight: 400;
+                      color: #000;
+                      text-align: center;
+                      padding: 0px 5px !important;
+                      margin: 0px 0px !important;
+                      border: 1px solid #000;
+                      height: 5px;
+                    ">
+                   ADV Amt
+                </td>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td style="
+                      font-size: 10px;
+                      line-height: 14px;
+                      font-weight: 400;
+                      color: #000;
+                      text-align: center;
+                      padding: 0px 5px !important;
+                      margin: 0px 0px !important;
+                      border: 1px solid #000;
+                      height: 5px;
+                    ">
+                   1
+                </td>
+                <td style="
+                      font-size: 10px;
+                      line-height: 14px;
+                      font-weight: 400;
+                      color: #000;
+                      text-align: center;
+                      padding: 0px 5px !important;
+                      margin: 0px 0px !important;
+                      border: 1px solid #000;
+                      height: 5px;
+                    ">
+                    CH0026
+                </td>
+                <td style="
+                      font-size: 10px;
+                      line-height: 14px;
+                      font-weight: 400;
+                      color: #000;
+                      text-align: center;
+                      padding: 0px 5px !important;
+                      margin: 0px 0px !important;
+                      border: 1px solid #000;
+                      height: 5px;
+                    ">
+                   BuildUp
+                </td>
+                <td style="
+                      font-size: 10px;
+                      line-height: 14px;
+                      font-weight: 400;
+                      color: #000;
+                      text-align: center;
+                      padding: 0px 5px !important;
+                      margin: 0px 0px !important;
+                      border: 1px solid #000;
+                      height: 5px;
+                    ">
+                   125
+                </td>
+                <td style="
+                      font-size: 10px;
+                      line-height: 14px;
+                      font-weight: 400;
+                      color: #000;
+                      text-align: center;
+                      padding: 0px 5px !important;
+                      margin: 0px 0px !important;
+                      border: 1px solid #000;
+                      height: 5px;
+                    ">
+                   10/01/2024
+                </td>
+                <td style="
+                      font-size: 10px;
+                      line-height: 14px;
+                      font-weight: 400;
+                      color: #000;
+                      text-align: center;
+                      padding: 0px 5px !important;
+                      margin: 0px 0px !important;
+                      border: 1px solid #000;
+                      height: 5px;
+                    ">
+                  5000
+                </td>
+              </tr>
+              <tr>
+                <td style="
+                      font-size: 10px;
+                      line-height: 14px;
+                      font-weight: 400;
+                      color: #000;
+                      text-align: center;
+                      padding: 0px 5px !important;
+                      margin: 0px 0px !important;
+                      border: 1px solid #000;
+                      height: 5px;
+                    ">
+                   2
+                </td>
+                <td style="
+                      font-size: 10px;
+                      line-height: 14px;
+                      font-weight: 400;
+                      color: #000;
+                      text-align: center;
+                      padding: 0px 5px !important;
+                      margin: 0px 0px !important;
+                      border: 1px solid #000;
+                      height: 5px;
+                    ">
+                    CH0026
+                </td>
+                <td style="
+                      font-size: 10px;
+                      line-height: 14px;
+                      font-weight: 400;
+                      color: #000;
+                      text-align: center;
+                      padding: 0px 5px !important;
+                      margin: 0px 0px !important;
+                      border: 1px solid #000;
+                      height: 5px;
+                    ">
+                   BuildUp
+                </td>
+                <td style="
+                      font-size: 10px;
+                      line-height: 14px;
+                      font-weight: 400;
+                      color: #000;
+                      text-align: center;
+                      padding: 0px 5px !important;
+                      margin: 0px 0px !important;
+                      border: 1px solid #000;
+                      height: 5px;
+                    ">
+                   125
+                </td>
+                <td style="
+                      font-size: 10px;
+                      line-height: 14px;
+                      font-weight: 400;
+                      color: #000;
+                      text-align: center;
+                      padding: 0px 5px !important;
+                      margin: 0px 0px !important;
+                      border: 1px solid #000;
+                      height: 5px;
+                    ">
+                   10/01/2024
+                </td>
+                <td style="
+                      font-size: 10px;
+                      line-height: 14px;
+                      font-weight: 400;
+                      color: #000;
+                      text-align: center;
+                      padding: 0px 5px !important;
+                      margin: 0px 0px !important;
+                      border: 1px solid #000;
+                      height: 5px;
+                    ">
+                  5000
+                </td>
+              </tr>
+              <tr>
+                <td style="
+                      font-size: 10px;
+                      line-height: 14px;
+                      font-weight: 400;
+                      color: #000;
+                      text-align: center;
+                      padding: 0px 5px !important;
+                      margin: 0px 0px !important;
+                      border: 1px solid #000;
+                      height: 5px;
+                    ">
+                </td>
+                <td style="
+                      font-size: 10px;
+                      line-height: 14px;
+                      font-weight: 400;
+                      color: #000;
+                      text-align: center;
+                      padding: 0px 5px !important;
+                      margin: 0px 0px !important;
+                      border: 1px solid #000;
+                      height: 5px;
+                    ">
+                </td>
+                <td style="
+                      font-size: 10px;
+                      line-height: 14px;
+                      font-weight: 400;
+                      color: #000;
+                      text-align: center;
+                      padding: 0px 5px !important;
+                      margin: 0px 0px !important;
+                      border: 1px solid #000;
+                      height: 5px;
+                    ">
+                </td>
+                <td style="
+                      font-size: 10px;
+                      line-height: 14px;
+                      font-weight: 400;
+                      color: #000;
+                      text-align: center;
+                      padding: 0px 5px !important;
+                      margin: 0px 0px !important;
+                      border: 1px solid #000;
+                      height: 5px;
+                    ">
+                </td>
+                <td style="
+                      font-size: 10px;
+                      line-height: 14px;
+                      font-weight: 400;
+                      color: #000;
+                      text-align: center;
+                      padding: 0px 5px !important;
+                      margin: 0px 0px !important;
+                      border: 1px solid #000;
+                      height: 5px;
+                      text-transform: uppercase;
+                    ">
+                   TOTAL
+                </td>
+                <td style="
+                      font-size: 10px;
+                      line-height: 14px;
+                      font-weight: 400;
+                      color: #000;
+                      text-align: center;
+                      padding: 0px 5px !important;
+                      margin: 0px 0px !important;
+                      border: 1px solid #000;
+                      height: 5px;
+                    ">
+                  10000
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</body>
+
+</html>
