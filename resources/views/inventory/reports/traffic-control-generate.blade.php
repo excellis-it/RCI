bb5573e654a29aa1418ee5bec0726909850d73ef
shreeja <shreeja@excellisit.net>
2024-08-12 15:44:06 -0700

    many chnages

--- resources/views/inventory/reports/traffic-control-generate.blade.php
+++ resources/views/inventory/reports/traffic-control-generate.blade.php
@@ -1,162 +1,145 @@
 <!DOCTYPE html>
 <html lang="en">
-  <title>RCI</title>
-  <meta charset="utf-8" />
+<title>RCI</title>
+<meta charset="utf-8" />
 
-  <body style="background: #fff">
-    <table
-      width="100%"
-      border="0"
-      cellpadding="0"
-      cellspacing="0"
-      align="center"
-    >
-      <tbody>
-        <tr>
-          <td>
-            <table
-              width="100%"
-              border="0"
-              cellpadding="0"
-              cellspacing="0"
-              align="center"
-            >
-              <tbody>
-                <tr>
-                  <td></td>
-                  <td></td>
+<body style="background: #fff">
+    <table width="100%" border="0" cellpadding="0" cellspacing="0" align="center">
+        <tbody>
+            <tr>
+                <td>
+                    <table width="100%" border="0" cellpadding="0" cellspacing="0" align="center">
+                        <tbody>
+                            <tr>
+                                <td></td>
+                                <td></td>
 
-                  <td style="font-size: 14px; text-align: right; font-weight: bold;">DRDO.SM.01</td>
-                </tr>
-                <tr>
-                  <td style="font-size: 14px"></td>
-                  <td
-                    style="
+                                <td style="font-size: 10px; text-align: right; font-weight: bold;">DRDO.SM.01</td>
+                            </tr>
+                            <tr>
+                                <td style="font-size: 14px"></td>
+                                <td
+                                    style="
                       text-align: center;
                       font-weight: bold;
-                      font-size: 14px;
+                      font-size: 10px;
                       width: 100%;
-                    "
-                  >
-                  (Name of Lab/ Estt)<br />
-                  TRAFFIC CONTROL REGISTER (TCR)
-                  </td>
-                  <td style="font-size: 14px; text-align: right"></td>
-                </tr>
-              </tbody>
-            </table>
-          </td>
-        </tr>
-        <tr>
-          <td>
-            <table
-              width="100%"
-              border="0"
-              cellpadding="0"
-              cellspacing="0"
-              align="center"
-              style="margin-top: 1rem"
-            >
-              <tbody>
-                <tr>
-                    <td></td>
-                  <td style="padding-top: 20px; font-weight: bold; text-align: center; ">
-                    (For items to be collected by CRDS)
-                  </td>
-                  <td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
+                    ">
+                                    (Name of Lab/ Estt)<br />
+                                    TRAFFIC CONTROL REGISTER (TCR)
+                                </td>
+                                <td style="font-size: 10px; text-align: right"></td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <table width="100%" border="0" cellpadding="0" cellspacing="0" align="center"
+                        style="margin-top: 1rem">
+                        <tbody>
+                            <tr>
+                                <td></td>
+                                <td style="padding-top: 20px; font-weight: bold; text-align: center; ">
+                                    (For items to be collected by CRDS)
+                                </td>
+                                <td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
 
-                </tr>
-               
-              </tbody>
-            </table>
-          </td>
-        </tr>
+                            </tr>
 
-        <tr>
-            <td>
-              <table
-                width="100%"
-                border="0"
-                cellpadding="0"
-                cellspacing="0"
-                align="center"
-                style="margin-top: 1rem"
-              >
-                <tbody>
-                  <tr>
-                    <th rowspan="2" style="border: 1px solid #000; padding: 5px; font-size: 14px;">TCR SI.No</th>
-                    <th colspan="2" style="border: 1px solid #000; padding: 5px; font-size: 14px;">LR/RR/AWB/BL/ APP/RPP</th>
-                    <th rowspan="2" style="border: 1px solid #000; padding: 5px; font-size: 14px;">Name of Consignor</th>
-                    <th rowspan="2" style="border: 1px solid #000; padding: 5px; font-size: 14px;">Name of Transporter/ Carrier</th>
-                    <th rowspan="2" style="border: 1px solid #000; padding: 5px; font-size: 14px;">Supply order No / Contract No. / Authority & Date</th>
+                        </tbody>
+                    </table>
+                </td>
+            </tr>
 
-                    
-                    <th rowspan="2" style="border: 1px solid #000; padding: 5px; font-size: 14px;">Date of Collection of Stores</th>
-                  
-                  </tr>
-                 
-                    <tr>
-                      <td style="border: 1px solid #000; padding: 5px; text-align: center;">No</td>
-                      <td style="border: 1px solid #000; padding: 5px; text-align: center;">Date</td>
-                
-                    </tr>
-                    <tr>
-                        <td style="border: 1px solid #000; padding: 5px; text-align: center;">1</td>
-                        <td style="border: 1px solid #000; padding: 5px; text-align: center;">2</td>
-                        <td style="border: 1px solid #000; padding: 5px; text-align: center;">3</td>
-                        <td style="border: 1px solid #000; padding: 5px; text-align: center;">4</td>
-                        <td style="border: 1px solid #000; padding: 5px; text-align: center;">5</td>
-                        <td style="border: 1px solid #000; padding: 5px; text-align: center;">6</td>
-                        <td style="border: 1px solid #000; padding: 5px; text-align: center;">7</td>
-               
-  
-                      </tr>
-                </tbody>
-              </table>
-            </td>
-          </tr>
-          <tr>
-            <td>
-              <table
-                width="100%"
-                border="0"
-                cellpadding="0"
-                cellspacing="0"
-                align="center"
-                style="margin-top: 5rem"
-              >
-                <tbody>
-                  <tr>
-             
-                    <th style="border: 1px solid #000; padding: 5px; font-size: 14px;">No. of
-                        Packages &
-                        Gross
-                        Weight</th>
-                    <th style="border: 1px solid #000; padding: 5px; font-size: 14px;">Condition of Package</th>
-                    <th style="border: 1px solid #000; padding: 5px; font-size: 14px;">Freight Pre Paid/ to Pay (Amount)</th>
+            <tr>
+                <td>
+                    <table width="100%" border="0" cellpadding="0" cellspacing="0" align="center"
+                        style="margin-top: 1rem">
+                        <tbody>
+                            <tr>
+                                <th rowspan="2" style="border: 1px solid #000; padding: 5px; font-size: 10px;">TCR
+                                    SI.No</th>
+                                <th colspan="2" style="border: 1px solid #000; padding: 5px; font-size: 10px;">
+                                    LR/RR/AWB/BL/ APP/RPP</th>
+                                <th rowspan="2" style="border: 1px solid #000; padding: 5px; font-size: 10px;">Name
+                                    of Consignor</th>
+                                <th rowspan="2" style="border: 1px solid #000; padding: 5px; font-size: 10px;">Name
+                                    of Transporter/ Carrier</th>
+                                <th rowspan="2" style="border: 1px solid #000; padding: 5px; font-size: 10px;">Supply
+                                    order No / Contract No. / Authority & Date</th>
+                                <th rowspan="2" style="border: 1px solid #000; padding: 5px; font-size: 10px;">Date
+                                    of Collection of Stores</th>
+                            </tr>
 
-                    <th style="border: 1px solid #000; padding: 5px; font-size: 14px;">Remarks</th>
-                    
-                    <th style="border: 1px solid #000; padding: 5px; font-size: 14px;">Name & Sign. Of CRDS Officer</th>
-                  
-                  </tr>
-                 
-                
-                    <tr>
-                        <td style="border: 1px solid #000; padding: 5px; text-align: center;">8</td>
-                        <td style="border: 1px solid #000; padding: 5px; text-align: center;">9</td>
-                        <td style="border: 1px solid #000; padding: 5px; text-align: center;">10</td>
-                        <td style="border: 1px solid #000; padding: 5px; text-align: center;">11</td>
-                        <td style="border: 1px solid #000; padding: 5px; text-align: center;">12</td>
-                   
-               
-  
-                      </tr>
-                </tbody>
-              </table>
-            </td>
-          </tr>
-      </tbody>
+                            <tr>
+                                <td style="border: 1px solid #000; padding: 5px; text-align: center;">No</td>
+                                <td style="border: 1px solid #000; padding: 5px; text-align: center;">Date</td>
+                            </tr>
+                            <tr>
+                                <td style="border: 1px solid #000; padding: 5px; text-align: center;">1</td>
+                                <td style="border: 1px solid #000; padding: 5px; text-align: center;">2</td>
+                                <td style="border: 1px solid #000; padding: 5px; text-align: center;">3</td>
+                                <td style="border: 1px solid #000; padding: 5px; text-align: center;">4</td>
+                                <td style="border: 1px solid #000; padding: 5px; text-align: center;">5</td>
+                                <td style="border: 1px solid #000; padding: 5px; text-align: center;">6</td>
+                                <td style="border: 1px solid #000; padding: 5px; text-align: center;">7</td>
+                            </tr>
+
+                            <tr>
+                                <td style="border: 1px solid #000; padding: 5px; text-align: center;">1</td>
+                                <td style="border: 1px solid #000; padding: 5px; text-align: center;">2</td>
+                                <td style="border: 1px solid #000; padding: 5px; text-align: center;">3</td>
+                                <td style="border: 1px solid #000; padding: 5px; text-align: center;">4</td>
+                                <td style="border: 1px solid #000; padding: 5px; text-align: center;">5</td>
+                                <td style="border: 1px solid #000; padding: 5px; text-align: center;">6</td>
+                                <td style="border: 1px solid #000; padding: 5px; text-align: center;">7</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <table width="100%" border="0" cellpadding="0" cellspacing="0" align="center"
+                        style="margin-top: 5rem">
+                        <tbody>
+                            <tr>
+
+                                <th style="border: 1px solid #000; padding: 5px; font-size: 10px;">No. of
+                                    Packages &
+                                    Gross
+                                    Weight</th>
+                                <th style="border: 1px solid #000; padding: 5px; font-size: 10px;">Condition of Package
+                                </th>
+                                <th style="border: 1px solid #000; padding: 5px; font-size: 10px;">Freight Pre Paid/ to
+                                    Pay (Amount)</th>
+
+                                <th style="border: 1px solid #000; padding: 5px; font-size: 10px;">Remarks</th>
+
+                                <th style="border: 1px solid #000; padding: 5px; font-size: 10px;">Name & Sign. Of CRDS
+                                    Officer</th>
+
+                            </tr>
+
+
+                            <tr>
+                                <td style="border: 1px solid #000; padding: 5px; text-align: center;">8</td>
+                                <td style="border: 1px solid #000; padding: 5px; text-align: center;">9</td>
+                                <td style="border: 1px solid #000; padding: 5px; text-align: center;">10</td>
+                                <td style="border: 1px solid #000; padding: 5px; text-align: center;">11</td>
+                                <td style="border: 1px solid #000; padding: 5px; text-align: center;">12</td>
+
+
+
+                            </tr>
+                        </tbody>
+                    </table>
+                </td>
+            </tr>
+        </tbody>
     </table>
-  </body>
+</body>
+
 </html>
