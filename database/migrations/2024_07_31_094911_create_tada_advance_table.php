874ee83cc4b75dc686fc2c2baf9941a4fced260e
unknown <web.excellisit@gmail.com>
2024-09-01 22:23:28 +0530

    many changes

--- database/migrations/2024_07_31_094911_create_tada_advance_table.php
+++ database/migrations/2024_07_31_094911_create_tada_advance_table.php
@@ -25,7 +25,7 @@ return new class extends Migration
             $table->float('claim_amount', 10, 2)->default(0.00);
             $table->float('pass_amount', 10, 2)->default(0.00);
             $table->string('mro')->nullable();
-            $table->float('due_amount', 10, 2);
+            $table->float('due_amount', 10, 2)->nullable();
             $table->tinyInteger('status')->comment('0=Pending, 1=Accepted');
             $table->timestamps(); // Handles created_at and updated_at fields
 
--- resources/views/frontend/layouts/master.blade.php
+++ resources/views/frontend/layouts/master.blade.php
@@ -25,8 +25,8 @@
     {{-- <link id="" rel="" href="{{ asset('frontend_assets/css/dselect.scss') }}"> --}}
     <link rel="stylesheet" type="text/css"
         href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/7.2.0/sweetalert2.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    {{-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/7.2.0/sweetalert2.min.css"> --}}
+    {{-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"> --}}
     @stack('styles')
 </head>
 
@@ -65,8 +65,8 @@
     <!-- <script src="js/dashboard.js"></script> -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/js/toastr.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.2/jquery.validate.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/7.2.0/sweetalert2.all.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+    {{-- <script src="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/7.2.0/sweetalert2.all.min.js"></script> --}}
+    {{-- <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script> --}}
     <script>
         window.addEventListener('load', () => {
             $('.select23').select2();
