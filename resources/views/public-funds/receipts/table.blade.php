d29224d468fe966a2c3929aad81c53d9e764f618
SouparnaBiswas <souparna.biswas@excellisit.net>
2024-12-16 17:07:08 +0530

    delete modal

--- resources/views/public-funds/receipts/table.blade.php
+++ resources/views/public-funds/receipts/table.blade.php
@@ -6,29 +6,43 @@
             <td>{{ $receipt->category->name ?? 'N/A' }}</td>
             <td>{{ $receipt->vr_date ?? 'N/A' }}</td>
             <td>{{ $receipt->amount ?? 'N/A' }}</td>
-            <td class="sepharate"><a data-route="{{ route('receipts.edit', $receipt->id) }}"
-                    href="#" onclick="getEditForm({{$receipt->id}})" class="edit_pencil"><i class="ti ti-pencil"></i></a>
-                    {{-- <a href="{{route('receipts.delete', ['vr_no' => $receipt->vr_no, 'vr_date' => $receipt->vr_date])}}" id="delete" class="delete" data-route="{{route('receipts.delete', ['vr_no' => $receipt->vr_no, 'vr_date' => $receipt->vr_date])}}"><i class="ti ti-trash"></i></a> --}}
-                    <a href="javascript:void(0);" id="delete" class="delete" data-bs-toggle="modal" data-bs-target="#deleteModal-{{$receipt->vr_no}}">
-                        <i class="ti ti-trash"></i>
-                    </a>
+            <td class="sepharate"><a data-route="{{ route('receipts.edit', $receipt->id) }}" href="#"
+                    onclick="getEditForm({{ $receipt->id }})" class="edit_pencil"><i class="ti ti-pencil"></i></a>
+                {{-- <a href="{{route('receipts.delete', ['vr_no' => $receipt->vr_no, 'vr_date' => $receipt->vr_date])}}" id="delete" class="delete" data-route="{{route('receipts.delete', ['vr_no' => $receipt->vr_no, 'vr_date' => $receipt->vr_date])}}"><i class="ti ti-trash"></i></a> --}}
+                {{-- <a href="javascript:void(0);" id="delete" class="delete" data-bs-toggle="modal"
+                    data-bs-target="#deleteModal-{{ $receipt->vr_no }}">
 
-                    <div class="modal fade" id="deleteModal-{{$receipt->vr_no}}" tabindex="-1" aria-hidden="true">
-                        <div class="modal-dialog modal-dialog-centered modal-md"> <!-- Adjust modal size dynamically -->
-                            <div class="modal-content bg-danger">
-                                <div class="modal-header">
-                                    <h5 class="modal-title text-white">Are you sure want to delete?</h5>
-                                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                                </div>
-                                <div class="modal-footer d-flex flex-wrap justify-content-between">
-                                    <button type="button" class="btn btn-light w-100 mb-2" data-bs-dismiss="modal" style="max-width: 200px;">Cancel</button>
-                                    <a href="{{ route('receipts.delete', ['vr_no' => $receipt->vr_no, 'vr_date' => $receipt->vr_date]) }}"
-                                       class="btn btn-dark w-100" style="max-width: 200px;">Confirm</a>
-                                </div>
-                            </div>
-                        </div>
+                </a> --}}
+                <a href="javascript:void(0);" class="delete-receipt"
+                    data-delete-url="{{ route('receipts.delete', ['vr_no' => $receipt->vr_no, 'vr_date' => $receipt->vr_date]) }}">
+                    <i class="ti ti-trash"></i>
+                </a>
 
-                    </div>
+                <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+                <script>
+                    document.addEventListener('DOMContentLoaded', function() {
+                        document.querySelectorAll('.delete-receipt').forEach(function(button) {
+                            button.addEventListener('click', function() {
+                                const deleteUrl = this.dataset.deleteUrl;
+
+                                Swal.fire({
+                                    title: 'Are you sure want to delete?',
+                                    text: "You won't be able to revert this!",
+                                    icon: 'warning',
+                                    showCancelButton: true,
+                                    confirmButtonColor: '#3085d6',
+                                    cancelButtonColor: '#d33',
+                                    confirmButtonText: 'Yes, delete it!'
+                                }).then((result) => {
+                                    if (result.isConfirmed) {
+                                        // Redirect to the delete URL
+                                        window.location.href = deleteUrl;
+                                    }
+                                });
+                            });
+                        });
+                    });
+                </script>
             </td>
         </tr>
     @endforeach
