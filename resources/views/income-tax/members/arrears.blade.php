40aaa66df3aba64d2852295f47153df807ec0b12
masum-excellisit <masum@excellisit.net>
2025-03-12 15:17:40 +0530

    income tax saving

---
+++ resources/views/income-tax/members/arrears.blade.php
@@ -0,0 +1,206 @@
+<div class="tab-pane fade" id="arrears" role="tabpanel" aria-labelledby="arrears-tab">
+    <form>
+        <div class="credit-frm">
+            <div class="row mb-3">
+                <div class="col-md-7">
+                    <div class="recov-table">
+                        <table class="table customize-table mb-0 align-middle bg_tbody" id="loan-table">
+                            <thead class="text-white fs-4 bg_blue">
+                                <tr>
+                                    <th>Date</th>
+                                    <th>Name</th>
+                                    <th>Amt</th>
+                                    <th>CPS</th>
+                                    <th>I.Tax</th>
+                                    <th>CGHS</th>
+                                    <th>GMC</th>
+                                </tr>
+                            </thead>
+                            <tbody class="tbody_height_scroll">
+                                <tr class="edit-route-loan">
+                                    <td>18/02/2025</td>
+                                    <td>DA2</td>
+                                    <td>20796</td>
+                                    <td>0</td>
+                                    <td>0</td>
+                                    <td>0</td>
+                                    <td>0</td>
+                                </tr>
+                                <tr class="edit-route-loan">
+                                    <td>18/02/2025</td>
+                                    <td>DA2</td>
+                                    <td>20796</td>
+                                    <td>0</td>
+                                    <td>0</td>
+                                    <td>0</td>
+                                    <td>0</td>
+                                </tr>
+                                <tr class="edit-route-loan">
+                                    <td>18/02/2025</td>
+                                    <td>DA2</td>
+                                    <td>20796</td>
+                                    <td>0</td>
+                                    <td>0</td>
+                                    <td>0</td>
+                                    <td>0</td>
+                                </tr>
+                                <tr class="edit-route-loan">
+                                    <td>18/02/2025</td>
+                                    <td>DA2</td>
+                                    <td>20796</td>
+                                    <td>0</td>
+                                    <td>0</td>
+                                    <td>0</td>
+                                    <td>0</td>
+                                </tr>
+                                <tr class="edit-route-loan">
+                                    <td>18/02/2025</td>
+                                    <td>DA2</td>
+                                    <td>20796</td>
+                                    <td>0</td>
+                                    <td>0</td>
+                                    <td>0</td>
+                                    <td>0</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+                <div class="col-md-5" id="loan-form">
+                    <div class="row">
+                        <div class="form-group col-md-6 mb-2">
+                            <div class="row align-items-center">
+                                <div class="col-md-12">
+                                    <label>Date</label>
+                                </div>
+                                <div class="col-md-12">
+                                    <input type="text" class="form-control" name="" id=""
+                                        value="">
+                                    <span class="text-danger"></span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="form-group col-md-6 mb-2">
+                            <div class="row align-items-center">
+                                <div class="col-md-12">
+                                    <label>Name</label>
+                                </div>
+                                <div class="col-md-12">
+                                    <input type="text" class="form-control" name="" id=""
+                                        value="">
+                                    <span class="text-danger"></span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="form-group col-md-6 mb-2">
+                            <div class="row align-items-center">
+                                <div class="col-md-12">
+                                    <label>Amt</label>
+                                </div>
+                                <div class="col-md-12">
+                                    <input type="text" class="form-control" name="" id=""
+                                        value="">
+                                    <span class="text-danger"></span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="form-group col-md-6 mb-2">
+                            <div class="row align-items-center">
+                                <div class="col-md-12">
+                                    <label>CPS</label>
+                                </div>
+                                <div class="col-md-12">
+                                    <input type="text" class="form-control" name="" id=""
+                                        value="">
+                                    <span class="text-danger"></span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="form-group col-md-6 mb-2">
+                            <div class="row align-items-center">
+                                <div class="col-md-12">
+                                    <label>I.Tax</label>
+                                </div>
+                                <div class="col-md-12">
+                                    <input type="text" class="form-control" name="" id=""
+                                        value="">
+                                    <span class="text-danger"></span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="form-group col-md-6 mb-2">
+                            <div class="row align-items-center">
+                                <div class="col-md-12">
+                                    <label>CGHS</label>
+                                </div>
+                                <div class="col-md-12">
+                                    <input type="text" class="form-control" name="" id=""
+                                        value="">
+                                    <span class="text-danger"></span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="form-group col-md-6 mb-2">
+                            <div class="row align-items-center">
+                                <div class="col-md-12">
+                                    <label>GMC</label>
+                                </div>
+                                <div class="col-md-12">
+                                    <input type="text" class="form-control" name="" id=""
+                                        value="">
+                                    <span class="text-danger"></span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row justify-content-end">
+                        <div class="col-md-12">
+                            <div class="row justify-content-end">
+                                <div class="form-group col-md-4 mb-2">
+                                    <button type="submit" class="listing_add">Save</button>
+                                </div>
+                                <div class="form-group col-md-4 mb-2">
+                                    <button type="reset" class="listing_exit">Delete</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row justify-content-end mt-4">
+            <div class="col-auto mb-2">
+                <button type="submit" class="listing_add">Save</button>
+            </div>
+            <div class="col-auto mb-2">
+                <button type="button" class="another-btn">Another</button>
+            </div>
+            <div class="col-auto mb-2">
+                <button type="reset" class="listing_exit">Cancel</button>
+            </div>
+            <div class="col-auto mb-2">
+                <button type="button" class="another-btn">Report</button>
+            </div>
+            <div class="col-auto mb-2">
+                <button type="button" class="another-btn">FORM16</button>
+            </div>
+            <div class="col-auto mb-2">
+                Recovey Form
+                <select class="p-2 rounded">
+                    <option>Jan</option>
+                    <option>Feb</option>
+                    <option>Mar</option>
+                    <option>Apr</option>
+                    <option>May</option>
+                    <option>Jun</option>
+                    <option>Jul</option>
+                    <option>Aug</option>
+                    <option>Sep</option>
+                    <option>Oct</option>
+                    <option>Nov</option>
+                    <option>Dec</option>
+                </select>
+            </div>
+        </div>
+    </form>
+</div>
